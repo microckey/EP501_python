bfb07479fc5fa2c50089a70dd49967a585eb773a
Matthew Zettergren <mattzett@gmail.com>
2020-08-20 16:42:21 -0400

    starting to add pivoting

--- linear_algebra/Gauss_elim_example.py
+++ linear_algebra/Gauss_elim_example.py
@@ -7,19 +7,24 @@ Created on Thu Aug 20 15:40:51 2020
 """
 
 import numpy as np
-#from elimtools import Gauss_elim,backsub
-import elimtools
+from elimtools import Gauss_elim,backsub
 
-#nrow=4
-#ncol=4
-#A=np.random.randn(nrow,ncol)
-#b=np.random.randn(nrow,1)
+nrow=4
+ncol=4
+A=np.random.randn(nrow,ncol)
+b=np.random.randn(nrow,1)
 
-# Define test problem
-A=np.array([[1.0, 4.0, 2.0], [3.0, 2.0, 1.0], [2.0, 1.0, 3.0]])    # system to be solved
-b=np.array([[15.0], [10.0], [13.0]])                               # RHS of system
+## Simple test problem for debugging
+#A=np.array([[1.0, 4.0, 2.0], [3.0, 2.0, 1.0], [2.0, 1.0, 3.0]])    # system to be solved
+#b=np.array([[15.0], [10.0], [13.0]])                               # RHS of system
 
-Awork=elimtools.Gauss_elim(A,b)
-x=elimtools.backsub(Awork)
-print("Final value of x:  ")
+# Solve with the module we've coded
+Awork=Gauss_elim(A,b,True)
+x=backsub(Awork,True)
+print("Value of x computed via Gaussian elimination and backsubstitution:  ")
 print(x)
+
+# Use built-in linear algebra routines to solve and compare
+xpyth=np.linalg.solve(A,b);
+print("Solution vector computed via built-in numpy routine")
+print(xpyth)
--- linear_algebra/elimtools.py
+++ linear_algebra/elimtools.py
@@ -7,48 +7,63 @@ Created on Tue Aug 18 15:36:20 2020
 """
 
 import numpy as np
+import sys
+
 
 # Perform Gaussian elimination
-def Gauss_elim(A,b):
+def Gauss_elim(A,b,verbose):
 
-    # create and print the work array
-    [nrow,ncol]=A.shape   
+    # system size and error checking
+    [nrow,ncol]=A.shape
+    if (nrow == ncol):
+        n=nrow
+    else:
+        sys.exit("Matrix to be solved must be square")
+    
+    # create the work array for elimination
     Awork=np.concatenate((A,b),axis=1)               # composite work array/matrix; will be overwritten
-    print("Initial state of work matrix")
-    print(Awork)
-    
-    
+    if (verbose):
+        print("Initial state of work matrix")
+        print(Awork)
+        
+    # ordering array for pivoting and scaling
+    # order=np.arange(0,n,1)
+    # print (order)
+        
     # Simple Elimination example (no pivoting or scaling)
-    for i in range(0,nrow-1):            # row being used for elimination, note latter argument of range is number of iterations
+    for i in range(0,n-1):            # row being used for elimination, note latter argument of range is number of iterations
         pivel=Awork[i,i]                 # pivot element
-        for j in range(i+1,nrow):        # row we are eliminating from
+        for j in range(i+1,n):        # row we are eliminating from
             elimel=Awork[j,i]                 # lead factor to be eliminated
             Awork[j,i]=0                      # forcing this to zero avoid precision issues
-            for k in range(i+1,ncol+1):    # column elements, make sure to interate into the solution vector
+            for k in range(i+1,n+1):    # column elements, make sure to interate into the solution vector
                 Awork[j,k]=Awork[j,k]-elimel/pivel*Awork[i,k]
-                print("Present iteration of elimination",i,j,k)
-                print(pivel)
-                print(elimel)
-                print(Awork)
+                if (verbose):
+                    print("Present iteration of elimination",i,j,k)
+                    print(pivel)
+                    print(elimel)
+                    print(Awork)
                 
-    return Awork
+    return [Awork,order]
                 
 
 # execute backsubstitution
-def backsub(Awork):
+def backsub(Awork,verbose):
         
     [nrow,ncol1]=Awork.shape
     x=np.copy(Awork[:,-1]);
-    print("Initial value of solution vector")
-    print(x)
+    if (verbose):
+        print("Initial value of solution vector")
+        print(x)
     
     for i in range(nrow-1,-1,-1):
         denom=Awork[i,i]
         x[i]=x[i]/denom
         for j in range(i+1,ncol1-1):
              x[i]=x[i]-Awork[i,j]/denom*x[j]
-             print("Present value of solution vector",i,j)
-             print(x)
+             if (verbose):
+                 print("Present value of solution vector",i,j)
+                 print(x)
     return x
              
     
