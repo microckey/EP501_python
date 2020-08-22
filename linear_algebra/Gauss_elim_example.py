178ecf7229a0efdf20b3adf25cf947953f2b7784
Matthew Zettergren <mattzett@gmail.com>
2020-08-22 12:04:40 -0400

    updates docs

--- linear_algebra/Gauss_elim_example.py
+++ linear_algebra/Gauss_elim_example.py
@@ -4,6 +4,9 @@
 Created on Thu Aug 20 15:40:51 2020
 
 @author: zettergm
+
+known issues:  
+1)  Need to control number of decimal places in output printing to improve readability
 """
 
 import numpy as np
@@ -18,7 +21,7 @@ b=np.random.randn(nrow,1)
 #A=np.array([[1.0, 4.0, 2.0], [3.0, 2.0, 1.0], [2.0, 1.0, 3.0]])    # system to be solved
 #b=np.array([[15.0], [10.0], [13.0]])                               # RHS of system
 
-# Solve with the module we've coded
+# Solve with elimtools
 [Awork,order]=Gauss_elim(A,b,True)
 x=backsub(Awork[order,:],True)
 print("Value of x computed via Gaussian elimination and backsubstitution:  ")
