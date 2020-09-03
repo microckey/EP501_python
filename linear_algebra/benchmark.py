d1376d7649148ea5a26ec37485ecdac80449d8e8
Matthew Zettergren <mattzett@gmail.com>
2020-09-03 08:57:30 -0400

    started benchmarking demo

--- linear_algebra/benchmark.py
+++ linear_algebra/benchmark.py
@@ -8,3 +8,18 @@ Tests the performance of Gaussian elimination for systems of a variety of sizes
 @author: zettergm
 """
 
+# imports
+import numpy as np
+
+
+# define a range of system sizes to investigate
+nvals=np.arange(50,550,50)        # note arange excluded the end of the range
+testtime=np.zeros(nvals.size)     # time taken for each system size
+lrep=5                            # number of times to repeat each step (for consistency)
+
+for n in nvals:
+    A=np.random.randn(n,n)
+    b=np.random.randn(n,1)
+    
+    for irep in range(0,lrep):
+        
